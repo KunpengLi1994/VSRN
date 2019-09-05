5a60fcb419d594e1e5b6e01df6733b2b49480270
Kunpeng Li <kinpeng.li.1994@gmail.com>
2019-09-05 16:36:27 -0400

    Add files via upload

---
+++ GCN_lib/Rs_GCN.py
@@ -0,0 +1,85 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+
+class Rs_GCN(nn.Module):
+
+    def __init__(self, in_channels, inter_channels, bn_layer=True):
+        super(Rs_GCN, self).__init__()
+
+        self.in_channels = in_channels
+        self.inter_channels = inter_channels
+
+        if self.inter_channels is None:
+            self.inter_channels = in_channels // 2
+            if self.inter_channels == 0:
+                self.inter_channels = 1
+
+
+        conv_nd = nn.Conv1d
+        max_pool = nn.MaxPool1d
+        bn = nn.BatchNorm1d
+
+        self.g = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                         kernel_size=1, stride=1, padding=0)
+
+        if bn_layer:
+            self.W = nn.Sequential(
+                conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                        kernel_size=1, stride=1, padding=0),
+                bn(self.in_channels)
+            )
+            nn.init.constant(self.W[1].weight, 0)
+            nn.init.constant(self.W[1].bias, 0)
+        else:
+            self.W = conv_nd(in_channels=self.inter_channels, out_channels=self.in_channels,
+                             kernel_size=1, stride=1, padding=0)
+            nn.init.constant(self.W.weight, 0)
+            nn.init.constant(self.W.bias, 0)
+
+        self.theta = None
+        self.phi = None
+
+
+        self.theta = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+        self.phi = conv_nd(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+
+
+
+    def forward(self, v):
+        '''
+        :param v: (b, c, t, h, w)
+        :return:
+        '''
+        batch_size = v.size(0)
+
+        g_v = self.g(v).view(batch_size, self.inter_channels, -1)
+        g_v = g_v.permute(0, 2, 1)
+
+        theta_v = self.theta(v).view(batch_size, self.inter_channels, -1)
+        theta_v = theta_v.permute(0, 2, 1)
+        phi_v = self.phi(v).view(batch_size, self.inter_channels, -1)
+        R = torch.matmul(theta_v, phi_v)
+        N = R.size(-1)
+        R_div_C = R / N
+
+        y = torch.matmul(R_div_C, g_v)
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *v.size()[2:])
+        W_y = self.W(y)
+        v_star = W_y + v
+
+        return v_star
+
+
+
+
+
+
+
+
