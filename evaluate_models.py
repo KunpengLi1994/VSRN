99d55467e935923571f131dd37fe21c9f1e5bf44
Kunpeng Li <kinpeng.li.1994@gmail.com>
2019-09-05 17:24:13 -0400

    Initial commit

--- evaluate_models.py
+++ evaluate_models.py
@@ -3,4 +3,4 @@ from vocab import Vocabulary
 import evaluation_models
 
 
-evaluation_models.evalrank("pretrain_model/coco/model_coco_1.pth.tar", "pretrain_model/coco/model_coco_2.pth.tar", data_path='../SCAN_my/data/', split="testall", fold5=True)
+evaluation_models.evalrank("pretrain_model/coco/model_coco_1.pth.tar", "pretrain_model/coco/model_coco_2.pth.tar", data_path='data/', split="testall", fold5=True)
