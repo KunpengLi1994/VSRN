42f7e5d8c23e7a26adba5b3b1ba40667165d7c1e
Kunpeng Li <kinpeng.li.1994@gmail.com>
2019-09-05 17:07:03 -0400

    Initial commit

---
+++ misc/utils.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+# Input: seq, N*D numpy array, with element 0 .. vocab_size. 0 is END token.
+def decode_sequence(ix_to_word, seq):
+    seq = seq.cpu()
+    N, D = seq.size()
+    out = []
+    for i in range(N):
+        txt = ''
+        for j in range(D):
+            ix = seq[i, j].item()
+            if ix > 0:
+                if j >= 1:
+                    txt = txt + ' '
+                txt = txt + ix_to_word[str(ix)]
+            else:
+                break
+        out.append(txt)
+    return out
+
+
+class RewardCriterion(nn.Module):
+
+    def __init__(self):
+        super(RewardCriterion, self).__init__()
+
+    def forward(self, input, seq, reward):
+        input = input.contiguous().view(-1)
+        reward = reward.contiguous().view(-1)
+        mask = (seq > 0).float()
+        mask = torch.cat([mask.new(mask.size(0), 1).fill_(1).cuda(),
+                         mask[:, :-1]], 1).contiguous().view(-1)
+        output = - input * reward * mask
+        output = torch.sum(output) / torch.sum(mask)
+
+        return output
+
+
+class LanguageModelCriterion(nn.Module):
+
+    def __init__(self):
+        super(LanguageModelCriterion, self).__init__()
+        self.loss_fn = nn.NLLLoss(reduce=False)
+
+    def forward(self, logits, target, mask):
+        """
+        logits: shape of (N, seq_len, vocab_size)
+        target: shape of (N, seq_len)
+        mask: shape of (N, seq_len)
+        """
+        # truncate to the same size
+        batch_size = logits.shape[0]
+        target = target[:, :logits.shape[1]]
+        mask = mask[:, :logits.shape[1]]
+        logits = logits.contiguous().view(-1, logits.shape[2])
+        target = target.contiguous().view(-1)
+        mask = mask.contiguous().view(-1)
+        loss = self.loss_fn(logits, target)
+        output = torch.sum(loss * mask) / batch_size
+        return output
